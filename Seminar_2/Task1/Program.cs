bc51c258ca056e0dc78589485e19601163af65ec
Anna <shtefanova.1996@mail.ru>
2023-12-26 09:59:30 +0300

    Прописали подробное решение для второй задачи

--- Seminar_2/Task1/Program.cs
+++ Seminar_2/Task1/Program.cs
@@ -7,25 +7,25 @@
 // a = 891 => 81
 
 // //----------Вариант решения 1------------------
-// int num = new Random().Next(100, 1000); //new Random().Next(100, 999)- где 100 входит в промежуток, 
-//                                     //а 999 нет, поэтому пишем 1000
+int num = new Random().Next(100, 1000); //new Random().Next(100, 999)- где 100 входит в промежуток, 
+                                    //а 999 нет, поэтому пишем 1000
 
-// int num1 = num / 100; // остатка от деления в C# не будет,
-//                       // т.к. тип integer
-// int num2 = num % 10;
+int num1 = num / 100; // остатка от деления в C# не будет,
+                      // т.к. тип integer
+int num2 = num % 10;
 
-// Console.WriteLine(num);
+Console.WriteLine(num);
 // Console.WriteLine(num1 + "" + num2);
 // Console.WriteLine(num1 * 10 + num2);
 
 // // Для красивого вывода:
-// Console.WriteLine($"{num} => {num1 * 10 + num2}");
+Console.WriteLine($"{num} => {num1 * 10 + num2}");
 
 //--------------Вариант решения 2-----------------------
-int num = new Random().Next(100, 1000);
+// int num = new Random().Next(100, 1000);
 
-string num1 = Convert.ToString(num / 100);
-string num2 = Convert.ToString(num % 10);
+// string num1 = Convert.ToString(num / 100);
+// string num2 = Convert.ToString(num % 10);
 
-Console.WriteLine(num);
-Console.WriteLine(num1 + num2);
+// Console.WriteLine(num);
+// Console.WriteLine(num1 + num2);
--- Seminar_2/Task2/Program.cs
+++ Seminar_2/Task2/Program.cs
@@ -9,29 +9,36 @@
 // 617 => 1
 
 //=============Решение 1=====================
-// //вводим число
-// using System.Runtime.ExceptionServices;
+//вводим число
+int input = int.Parse(Console.ReadLine());
+//Трехзначное число состоит (X - первое число, Y - второе число, Z - третье число).
+//XYZ = X*100+Y*10+Z. => Y=(XYZ-X*100-Z)/10
+//Получаем третью цифру (Z)
+int ThirdNumber = input % 10; // 123 % 10 = 12,3 => Z = 3.
+//Получаем вторую цифру
+int SecondNumber = (input - (input / 100 * 100) - input % 10) / 10;
 
-// int input = int.Parse(Console.ReadLine());
-// //Получаем третью цифру
-// int ThirdNumber=input%10;
-// //Получаем вторую цифру
-// //Число- XYZ = X*100+Y*10+Z. Y=(XYZ-X*100-Z)/10
-// int SecondNumber=(input-(input/100 * 100)- input%10)/10;
+// Разбор формулы второй переменной (Y):
+//      input / 100 * 100 = 100 - Означает: 
+// 1. input / 100 = 123 / 100 = 1,00 (а не 1,23 потому что тип переменной integer)
+// 2. input / 100 * 100 = 1,00 * 100 = 100
+//      input - (input / 100 * 100) = 123 - 100 = 23
+//      (input - (input / 100 * 100) - input % 10) = (123 - 100 - 3) = 20
+//      (input - (input / 100 * 100) - input % 10) / 10 = 20 / 10 = 2 - второе число получено
 
-// //Возводим в степень
-// Console.WriteLine("Третье число: "+ThirdNumber+" ,второе число: "+SecondNumber );
-// // Вот пока не изучил- как перевести int в Double? 
-// Console.WriteLine("Результат возведения в степень: "+ SecondNumber + "^ "+ThirdNumber+ " " +Math.Pow( SecondNumber,ThirdNumber ));
-// //Третье число в степени равной второму.
+//Выводим второе и третьи числа:
+Console.WriteLine("Второе число: " + SecondNumber + ", третье число: " + ThirdNumber);
+//Возводим третье число в степень, которая равна второму числу (Z ^ Y)
+Console.WriteLine("Результат возведения в степень: " + SecondNumber + " ^ " + ThirdNumber + 
+                  " = " + Math.Pow( SecondNumber,ThirdNumber ));
 
 //=============Решение 2=====================
-int n = new Random().Next(100, 1000);
-int a = n % 100;
-Console.WriteLine(a);
-a = a / 10;
-Console.WriteLine(a);
-int b = n % 10;
-Console.WriteLine(b);
-double c = Math.Pow(a, b);
-Console.WriteLine($"{n} => {a}^{b} => {c}");
+// int n = new Random().Next(100, 1000);
+// int a = n % 100;
+// Console.WriteLine(a);
+// a = a / 10;
+// Console.WriteLine(a);
+// int b = n % 10;
+// Console.WriteLine(b);
+// double c = Math.Pow(a, b);
+// Console.WriteLine($"{n} => {a}^{b} => {c}");
