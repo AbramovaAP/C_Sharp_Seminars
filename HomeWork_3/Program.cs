f99632602769f984d0d5fb06ca0cf33d54dec819
Anna <shtefanova.1996@mail.ru>
2024-01-14 15:00:21 +0300

    Add HomeWork_3 & Solved AutoTest

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -6,112 +6,93 @@
 //Выводится: 5
 
 //Мой рабочий вариант без случайного массива
-        int[] array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};; //Задали одномерный массив из 10 целых чисел. 
-        int counter = 0; //счетчик
-        int minRange = 0; //индекс числа 10
-        int maxRange = 0; //индекс числа 90
+
+//int[] numbers = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
 
 //Выведем массив для наглядности:
-        for(int i = 0; i < array.Length; i++){ 
-            Console.Write($"{array[i]} ");
-        } 
-        Console.WriteLine();// пробел
+//         for(int i = 0; i < array.Length; i++){ 
+//             Console.Write($"{array[i]} ");
+//         } 
+//         Console.WriteLine();// пробел
 
-//найдем индексы min и max элементов
-        for(int i = 0; i < array.Length; i++){ 
-            if (array[i] == 10) {
-                minRange = i;
-                Console.WriteLine(minRange);};
-            if (array[i] == 90) {
-                maxRange = i;
-                Console.WriteLine(maxRange);};
-        }
-//Подсчитаем кол-во элементов между 10 и 90, по примеру числа не входят в интервал
-        for(int i = 0; i < array.Length; i++){
-            if (i > minRange & i < maxRange) {
-                counter++; // counter = counter + 1
-            }
-        }
-        Console.WriteLine(counter);
+//int counter = 0; //счетчик
+//       int minRange = 10;
+//        int maxRange = 90;
+
+//Подсчитаем кол-во элементов между 10 и 90
+        // for(int i = 0; i < numbers.Length; i++){
+        //     if (numbers[i] >= minRange & numbers[i] <= maxRange) {
+        //         counter++; // counter = counter + 1
+        //     }
+        // }
+//         }
+//         Console.WriteLine(counter);
 
 
-================Решение для автотеста======================================
-using System;
-using System.Linq;
+//================Решение для автотеста 1======================================
+// using System;
+// using System.Linq;
 
-//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
-class UserInputToCompileForTest
-{
-    // Подсчет количества элементов массива, попадающих в заданный диапазон
-    // numbers - массив, в котором ведется подсчет
-    // minRange - минимальная граница диапазона
-    // maxRange - максимальная граница диапазона
-    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
-    {
-        int[] numbers = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; //Задали одномерный массив из 10 целых чисел. 
-        int counter = 0; //счетчик
-        //int minRange = 0; //индекс числа 10
-        //int maxRange = 0; //индекс числа 90
+// //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+// class UserInputToCompileForTest
+// { 
+//     // Подсчет количества элементов массива, попадающих в заданный диапазон
+//     // numbers - массив, в котором ведется подсчет
+//     // minRange - минимальная граница диапазона
+//     // maxRange - максимальная граница диапазона
+//     public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+//     {
+//     //Числа для проверки:
+// //20, 50, 10, 10, 60, 30, 70, 98, 20, 40
+// //10, 50, 10, 1, 1, 30, 70, 91, 1, 1 
+// //2, 4, 6, 8, 10, 30, 70, 100, 10, 10 
 
-//найдем индексы min и max элементов
-        for(int i = 0; i < numbers.Length; i++){ 
-            if (numbers[i] == 10) {
-                minRange = i;
-                //Console.WriteLine(minRange);
-                }
-            if (numbers[i] == 90) {
-                maxRange = i;
-                //Console.WriteLine(maxRange);
-                }
-        }
-//Подсчитаем кол-во элементов между 10 и 90, по примеру числа не входят в интервал
-        for(int i = 0; i < numbers.Length; i++){
-            if (i > minRange & i < maxRange) {
-                counter++; // counter = counter + 1
-            }
-        }
-       //Console.WriteLine(counter);   
-    }
+//         int counter = 0; //счетчик
+//         minRange = 10;
+//         maxRange = 90;
 
-    public static void PrintResult(int[] array)
-    {
+// //Подсчитаем кол-во элементов между 10 и 90
+//         for(int i = 0; i < numbers.Length; i++){
+//             if (numbers[i] >= minRange & numbers[i] <= maxRange) {
+//                 counter++; // counter = counter + 1
+//             }
+//         }
+//     return counter;
+//     }
+//     public static void PrintResult(int[] array)
+//     {
+//         Console.WriteLine(CountItemsRange(array, 10, 90));
+//     }
 
-         Console.WriteLine(counter);
-    }
-
-}
+// }
 
 
-//Не удаляйте и не меняйте класс Answer!
-class Answer
-{
-    public static void Main(string[] args)
-    {
-        int[] array;
+// //Не удаляйте и не меняйте класс Answer!
+// class Answer
+// {
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+        
 
+//         if (args.Length >= 1) {
+//             // Объединяем все аргументы командной строки в одну строку
+//             string joinedArgs = string.Join(" ", args);
 
-        if (args.Length >= 1)
-        {
-            // Объединяем все аргументы командной строки в одну строку
-            string joinedArgs = string.Join(" ", args);
-
-            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
-            array = joinedArgs.Split(", ")
-                                  .Select(int.Parse)
-                                  .ToArray();
-
-            // Теперь arr содержит преобразованные в целые числа из командной строки
-
-        }
-        else
-        {
-            // Если аргументов на входе нет
-            array = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 }; // Создание массива
-        }
-        UserInputToCompileForTest.PrintResult(array);
-    }
-}
-
+//             // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+//             array = joinedArgs.Split(", ")
+//                                   .Select(int.Parse)
+//                                   .ToArray();
+            
+//             // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+//         } else {
+//            // Если аргументов на входе нет
+//             array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+//         }
+//         UserInputToCompileForTest.PrintResult(array);
+//     }
+// }
 
 
 //===================================================================================================================================================================
@@ -136,7 +117,7 @@ class Answer
 //         } 
 //         Console.WriteLine();// пробел
 
-// //Подсчитаем кол-во элементов между 10 и 90, по примеру числа не входят в интервал
+// //Подсчитаем кол-во четных элементов
 //         for(int i = 0; i < array.Length; i++){
 //             if (array[i] % 2 == 0) {
 //                 counter++; // counter = counter + 1
@@ -145,7 +126,7 @@ class Answer
 //         Console.WriteLine(counter);
 
 
-//================Решение для автотеста======================================
+// //================Решение для автотеста======================================
 // using System;
 // using System.Linq;
 
@@ -156,13 +137,24 @@ class Answer
 //     // numbers - массив, в котором ведется подсчет
 //     public static int CountEvenItems(int[] numbers)
 //     {
-//         //Напишите свое решение здесь
+// //Напишите свое решение здесь
+//         int counter = 0; //счетчик
+
+// //Подсчитаем кол-во четных элементов
+//         for(int i = 0; i < numbers.Length; i++){
+//             if (numbers[i] % 2 == 0) {
+//                 counter++; // counter = counter + 1
+//             }
+//         }
+//         return counter;
 //     }
     
 //     public static void PrintResult(int[] array)
 //     {
 //         //Напишите свое решение здесь
+//         Console.WriteLine(CountEvenItems(array));
 //     }
+    
 // }
 
 // //Не удаляйте и не меняйте класс Answer!
@@ -186,7 +178,7 @@ class Answer
         
 //         } else {
 //            // Если аргументов на входе нет
-//             array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+//             array = new int[] {2, 8, 7, 3, 7, 3, 7, 7, 2, 4}; // Создание массива
 //         }
 //         UserInputToCompileForTest.PrintResult(array);
 //     }
@@ -235,58 +227,74 @@ class Answer
 
 
 
-// //================Решение для автотеста======================================
-// using System;
-// using System.Linq;
+//================Решение для автотеста======================================
+using System;
+using System.Linq;
 
-// //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
-// class UserInputToCompileForTest
-// { 
-//     // Разница между максимальным и минимальным элементами массива
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+ 
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        //Напишите свое решение здесь
+        double min = numbers[0];
+         for(int i = 0; i < numbers.Length; i++){ 
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
     
-//     // Нахождение минимума массива
-//     public static double FindMin(double[] numbers)
-//     {
-//         //Напишите свое решение здесь
-//     }
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        
+        //Напишите свое решение здесь
+        double max = numbers[0]; 
+        for(int i = 0; i < numbers.Length; i++){ 
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+         }
+         return max;
+    }
     
-//     // Нахождение максимума массива
-//     public static double FindMax(double[] numbers)
-//     {
-//         //Напишите свое решение здесь
-//     }
-    
-    
-    
-//     public static void PrintResult(double[] array)
-//     {
-//         //Напишите свое решение здесь
-//     }
-// }
+    public static void PrintResult(double[] array)
+    {
+//Напишите свое решение здесь
 
-// //Не удаляйте и не меняйте класс Answer!
-// class Answer
-// {
-//     public static void Main(string[] args)
-//     {
-//         double[] array;
+        double Result = FindMax(array) - FindMin (array);
+        Console.WriteLine(Result);
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
         
 
-//         if (args.Length >= 1) {
-//             // Объединяем все аргументы командной строки в одну строку
-//             string joinedArgs = string.Join(" ", args);
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
 
-//             // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
-//             array = joinedArgs.Split(", ")
-//                                   .Select(double.Parse)
-//                                   .ToArray();
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
             
-//             // Теперь arr содержит преобразованные в целые числа из командной строки
+            // Теперь arr содержит преобразованные в целые числа из командной строки
         
-//         } else {
-//            // Если аргументов на входе нет
-//             array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
-//         }
-//         UserInputToCompileForTest.PrintResult(array);
-//     }
-// }
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
