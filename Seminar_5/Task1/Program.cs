366287b8dae96e9e786b6bad4c5cede8d1db3881
Anna <shtefanova.1996@mail.ru>
2024-01-18 14:03:41 +0300

    Add seminar_5 & Task

--- Seminar_5/Task1/Program.cs
+++ Seminar_5/Task1/Program.cs
@@ -6,11 +6,24 @@
 //   4 3 4 1 =>       4 3 4 1
 //   2 9 5 4          2 9 25 4
 
+// Ввод размероа матрицы
 Console.WriteLine("Введите размер m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите размер n: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
+// Цикл для заполнения матрицы случайными числами
+int[,] arr = new int[m, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(1, 10);
+    }
+}
+
+//Функция для печати матрицы
 void PrintArr(int[,] arry)
 {
     for (int i = 0; i < arry.GetLength(0); i++)
@@ -23,6 +36,7 @@ void PrintArr(int[,] arry)
     }
 }
 
+//Функция для определения четности индексов и возведение элементов в квадрат
 int[,] Sqrt(int[,] arry)
 {
     for (int i = 0; i < arry.GetLength(0); i++)
@@ -38,17 +52,8 @@ int[,] Sqrt(int[,] arry)
     return arry;
 }
 
-int[,] arr = new int[m, n];
-
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        arr[i, j] = new Random().Next(2, 10);
-    }
-}
-PrintArr(arr);
-
-int[,] newArr = Sqrt(arr);
-Console.WriteLine();
-PrintArr(newArr);
+//Запускаем функцию PrintArr для печати исходной матрицы и измененной
+PrintArr(arr); //исходная матрица
+int[,] newArr = Sqrt(arr); //измененную матрицу сохраняем в новую переменную
+Console.WriteLine(); //пробел
+PrintArr(newArr); //измененная матрица
--- Seminar_5/Task2/Program.cs
+++ Seminar_5/Task2/Program.cs
@@ -6,3 +6,20 @@
 //   4 3 4 1 => 2 + 3 + 5 = 10
 //   2 9 5 4
 
+Console.WriteLine("Введите размер m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintArr(int[,] arry)
+{
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
