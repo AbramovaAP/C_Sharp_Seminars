f0e0bd269d406ef9d6b5384ead0a9a823b552d22
Anna <shtefanova.1996@mail.ru>
2024-01-20 18:34:49 +0300

    Solved Task3 with autotest

--- HomeWork_5/Task3/Program.cs
+++ HomeWork_5/Task3/Program.cs
@@ -10,8 +10,93 @@
 //      };
 //      Выводится: 1
 
-//==============================Мое решение======================
+//==============================Мое решение РАБОЧЕЕ======================
 
+// // Ввод размеров матрицы
+// Console.WriteLine("Введите количество строк m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// //Функция для печати матрицы
+// void PrintArr2d(int[,] arry)
+// {
+//     for (int i = 0; i < arry.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arry.GetLength(1); j++)
+//         {
+//             Console.Write($"{arry[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// // Функция для заполнения матрицы случайными числами
+// int[,] Generate2dArray(int x, int y)
+// {
+//     Random Any = new Random();
+//     int[,] array = new int[x, y];
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = Any.Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+
+
+// //Функция для подсчета суммы элементов по строкам матрицы
+// int[] FindMasterNumbersSum(int[,] MASS){
+//     int[] Array1  = new int [m]; //одномерный массив
+//     int sum = 0;
+//     for(int i = 0; i < MASS.GetLength(0); i++)
+//     {
+//         sum = 0;
+//         for (int j = 0; j < MASS.GetLength(1); j++) {
+//             sum = sum + MASS[i,j];
+//         }
+//         Array1[i] = sum;
+//         Console.Write($"{Array1[i]}\t");
+//     }
+//     Console.WriteLine();
+    
+//     return Array1;
+// }
+
+// //Функция для поиска индекса минимального элемента одномерного массива
+    
+// int MinIndex (int[] Minimum)
+// {
+//     int min = Minimum[0];
+//     int imin = 0;
+//     for(int i = 0; i < Minimum.Length; i++)
+//     {
+//         if (Minimum[i] < min) {
+//             min = Minimum[i];
+//             imin = i;
+//         }
+//     }
+//     //Console.WriteLine(min);
+//     Console.Write(imin);
+//     return imin;
+// }
+
+
+// int[,] array = Generate2dArray(m, n);
+// Console.WriteLine();
+
+// Console.WriteLine("Исходный двумерный массив:");
+// PrintArr2d(array);
+// Console.WriteLine();
+
+// Console.WriteLine("Новый одномерный массив:");
+// int[] array1 = FindMasterNumbersSum(array);
+
+// Console.WriteLine("Номер строки с минимальным элементом:");
+// MinIndex(array1);
+// Console.WriteLine();
 
 
 //====================Решение для автотеста======================
@@ -25,16 +110,52 @@ class UserInputToCompileForTest
     public static int[] SumRows(int[,] array)
     {
       //Напишите свое решение здесь
+      int m = array.GetLength(0);
+        int[] Array1  = new int [m]; //одномерный массив пока еще пустой
+        int sum = 0;
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = 0;
+            for (int j = 0; j < array.GetLength(1); j++) {
+                sum = sum + array[i,j];
+            }
+            Array1[i] = sum;
+            //Console.Write($"{Array1[i]}\t");
+        }
+        //Console.WriteLine();
+        
+        return Array1;
     }
     
-    // Получение индекса минимального элемента в одномерном массиве
+    // Получение индекса минимального элемента в одномерном массиве == номеру строки в двумерном массиве
     public static int MinIndex(int[] array)
     {
        //Напишите свое решение здесь
+        int min = array[0];
+        int imin = 0;
+        for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min) {
+                    min = array[i];
+                    imin = i;
+                }
+            }
+        Console.Write(imin);
+        return imin;
     }
     public static void PrintResult(int[,] numbers)
     {   
        //Напишите свое решение здесь
+        // Console.WriteLine("Исходный двумерный массив:");
+        // Console.Write(numbers);
+        // Console.WriteLine();
+
+        // Console.WriteLine("Новый одномерный массив:");
+         int[] array1 = SumRows(numbers);
+
+        //Console.WriteLine("Номер строки с минимальным элементом:");
+        MinIndex(array1);
+        //Console.WriteLine();
     }
 }
 
