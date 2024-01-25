a70e841002cad358276f024b2158798c7d921307
Anna <shtefanova.1996@mail.ru>
2024-01-25 18:01:04 +0300

    Add HomeWork & Task1-4

--- HomeWork_6/Task2/Program.cs
+++ HomeWork_6/Task2/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
--- HomeWork_6/Task3/Program.cs
+++ HomeWork_6/Task3/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
