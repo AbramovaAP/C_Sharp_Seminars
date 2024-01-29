a4bbbf82e2dffb6ff100afeb7bc131d8477d7925
Anna <shtefanova.1996@mail.ru>
2024-01-29 23:27:19 +0300

    Solved Task3

--- HomeWork_7/Task3/Program.cs
+++ HomeWork_7/Task3/Program.cs
@@ -7,3 +7,34 @@
 //          [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
 
 //============= Решение ==============
+
+int Revers(int[] a, int i = 0)
+{
+    int n = a.Length;
+    if (i < n)
+    {
+        Console.Write($"{a[n - 1 - i]} ");
+        i++; 
+    }
+    else
+    {return 0;}
+    return Revers(a, i);
+}
+
+int n = 5;
+Random rand = new Random();
+int[] a = new int[n];
+
+Console.WriteLine("исходный массив:");
+for (int j = 0; j < n; j++)
+{
+    a[j] = rand.Next(-5, 11);
+    Console.Write($"{a[j]} ");
+}
+
+
+int i = 0;
+Console.WriteLine();
+Console.WriteLine("перевернутый массив:");
+Revers(a, i);
+//=========================================================================
