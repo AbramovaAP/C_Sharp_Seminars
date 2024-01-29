d41977fbafccc3d4a4844bc3e7dd81f98916fcfb
Anna <shtefanova.1996@mail.ru>
2024-01-29 19:32:14 +0300

    Solved Task1

--- Seminar_7/Task1/Program.cs
+++ Seminar_7/Task1/Program.cs
@@ -12,7 +12,7 @@
 int SumNum (int num)
 {
     //Введенное число с консоли  /10 
-    if (num == 0) {return 0;} //можно вернуть тут и num, все равно вернется правильное решение
+    if (num == 0) {return 0;} //можно вернуть тут и num, все равно 
     int result = num % 10 + SumNum (num / 10);
     return result;
 }
